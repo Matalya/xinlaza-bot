5316186b7f95012c84711d3f5ea8b3d789d06620
Matalya <matalya8@gmail.com>
2024-10-06 12:33:22 -0300

    Full refactor of the search functionality and added QoL features

--- xinlaza.js
+++ xinlaza.js
@@ -5,7 +5,10 @@ import * as raw_dictionary from './dictionary.json' with {type: "json"}
 const config = raw_config.default; //cuz js is stupid
 const token = config.token;
 const logs_channel = config.logs_channel;
+const xinlaza_esan = config.xinlaza_only;
+const PAGE_LENGTH = config.PAGE_LENGTH;
 let dictionary = raw_dictionary.default; //cuz js is stupid²
+let dictLen = dictionary.length;
 // Create a new client instance
 const client = new Client({
     intents: [
@@ -19,7 +22,7 @@ const client = new Client({
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+	console.log(`${readyClient.user.tag}, verkdes`);
 });
 
 function POS_abbrv(string) {
@@ -49,96 +52,225 @@ function POS_abbrv(string) {
     }
     return "[unknown type]";
 }
-
-function search_xinlaza(query){
-    let acc = "";
-    let counter = 0;
-
-    for (let i = 0; i < dictionary.length; i++) {
-        let word = dictionary[i];
-        //exact match
-        if (word.xinlaza.toLowerCase() === query.toLowerCase()) {
-            acc = `**exact match**:\n**${word.xinlaza}** _${POS_abbrv(word.type)}_: ${word.translation}\n\n` + acc;
-            continue;
-        }
-        //partial match
-        if (word.xinlaza.toLowerCase().includes(query.toLowerCase())) {
-            counter += 1;
-            acc += `**${word.xinlaza}** _${POS_abbrv(word.type)}_: ${word.translation}\n`;
-        }
+String.prototype.startsWith = function (substring) {
+    if (substring.length > this.length) {
+        return false
     }
 
-    if (acc == "") {
-        return "No matches found :'c"
+   return this.slice(0, substring.length) === substring;
+}
+
+String.prototype.endsWith = function (substring) {
+    if (substring.length > this.length) {
+        return false
+    }
+
+    return this.slice(this.length - substring.length, this.length) === substring
+}
+
+Array.prototype.joinIntoStr = function (separator = " ", maxLength = this.length) {
+    return this.slice(0, maxLength).map(item => String(item)).join(separator);
+}
+
+function xinlaza(args){
+    //parse the arguments
+    let type = "";
+    let start = "";
+    let end = "";
+    let page = 1;
+    let query = [];
+    for (let i = 0; i < args.length; i++) {
+        let arg = args[i];
+        let argEnd = arg.length
+        if (arg.startsWith("type:")) {
+            type = arg.slice(5, argEnd)
+        } else
+        if (arg.startsWith("start:")) {
+            start = arg.slice(6, argEnd)
+        } else
+        if (arg.startsWith("end:")) {
+            end = arg.slice(4, argEnd)
+        } else
+        if (arg.startsWith("page:")) {
+            page = Number(arg.slice(5, argEnd));
+        } else {
+            query.push(arg);
+        }
+    }
+    if (query.length === 0) {
+        query.push(start);
+    }
+
+    let acc = "";
+    let matchArr = [];
+    //begin to check words. TODO: implement binary search
+    for (let i = 0; i < dictLen; i++) {
+        let entry = dictionary[i];
+        let perfect_match = true;
+        let dictEntry = entry.xinlaza.split(" ");
+
+        //check for perfect matches, add it to the top of acc if one is found
+        if (query.length === dictEntry.length) {
+            for (let i = 0; i < query.length; i++) {
+                if (query[i].toLowerCase() !== dictEntry[i].toLowerCase()) {
+                    perfect_match = false;
+                    break;
+                }
+            }
+        } else {
+            perfect_match = false;
+        }
+        if (perfect_match) {
+            console.log(typeof matchArr)
+            matchArr.unshift(`**perfect match**:\n**${entry.xinlaza}** _${POS_abbrv(entry.type)}_: ${entry.translation}\n\n`);
+            continue;
+        }
+
+        //check for partial matches, add 'em to acc if one is found
+        let partial_match = query.some((queryItem) => {
+            //iterates over every word in the query and checks if it's present in the dictionary entry
+            return entry.xinlaza.toLowerCase().includes(queryItem.toLowerCase());
+        });
+        if (partial_match &&
+            entry.xinlaza.startsWith(start) &&
+            entry.xinlaza.endsWith(end) &&
+            (entry.type === type || type === "")
+        ) {
+            matchArr.push(`**${entry.xinlaza}** _${POS_abbrv(entry.type)}_: ${entry.translation}`);
+        }
+    } //dictionary iterating loop end
+
+    if (matchArr.length > 50) {
+        let page_lower = PAGE_LENGTH * (page - 1);
+        let page_upper = PAGE_LENGTH * page - 1;
+        acc = `-# Showing results ${page_lower + 1} to ${page_upper + 1} out of ${matchArr.length}\n` + matchArr.slice(page_lower, page_upper).join("\n");
+    } else if (matchArr.length === 1) {
+        acc = `-# Showing ${matchArr.length} result${matchArr > 1 ? "s" : ""}\n` + matchArr.join("\n");
+    } else {
+        return "No matches found :'c";
     }
     return acc;
 }
 
-function search_english(query){
-    let acc = "";
-
-    for (let i = 0; i < dictionary.length; i++) {
-        let word = dictionary[i];
-        if (word.translation.toLowerCase().includes(` ${query.toLowerCase()} `)) {
-            acc = `**${word.xinlaza}** _${POS_abbrv(word.type)}_: ${word.translation}\n` + acc;
+function english(args){
+    let type = "";
+    let page = 1;
+    let queryArray = [];
+    for (let i = 0; i < args.length; i++) {
+        let arg = args[i];
+        let argEnd = arg.length
+        if (arg.startsWith("type:")) {
+            type = arg.slice(5, argEnd)
+        } else
+        if (arg.startsWith("page:")) {
+            page = Number(arg.slice(5, argEnd));
+        } else {
+            query.push(arg);
         }
     }
 
-    if (acc == "") {
-        return "No matches found :'c"
+    //dictionayr search loop
+    let acc = "";
+    let matchArr = [];
+    for (let i = 0; i < dictLen; i++) {
+        let entry = dictionary[i];
+        let entryArray = entry.translation.split(/\W+/);
+        let match = false;
+        let matchArr = [];
+        if (type === entry.type || type === "") {
+            if (queryArray.length === 0) {
+                match = true;
+            } else {
+                for (let q = 0; q < queryArray.length; q++) {
+                    for (let e = 0; e < entryArray.length; e++) {
+                        if (entryArray[e] === queryArray[q]) {
+                            match = true;
+                        }
+                    }
+                }
+            }
+        }
+        if (match) {
+            matchArr.push(`**${entry.xinlaza}** _${POS_abbrv(entry.type)}_: ${entry.translation}`);
+        }
+    }//dictionary search loop end
+
+    if (matchArr.length > 50) {
+        let page_lower = PAGE_LENGTH * (page - 1)
+        let page_upper = PAGE_LENGTH * page
+        acc = `-# Showing results ${pager_lower + 1} to ${page_upper + 1}\n` + matchArr.slice(page_lower, page_upper).join("\n");
+    } else {
+        acc = `-# Showing ${matchArr.length} results\n` + matchArr.join("\n");
+    }
+
+    if (acc === "") {
+        return "No matches found :'c";
     }
     return acc;
+}
+
+function send(channel, message) {
+    channel.send(message.slice(0, 2000))
+    if (message.length >= 2000) {
+        console.log(`Message was clipped from ${message.length} characters to avoid an application crash`);
+    }
 }
 
 client.on('messageCreate', (message) => {
-    if (message.author.tag === client.user.tag) {
+if (message.author.tag === client.user.tag) {
+    return;
+}
+
+let content = message.content;
+let author = message.author;
+let channel = message.channel;
+let time = message.createdTimestamp / 1000;
+let msg_url = message.url;
+let attachments_array = Array.from(message.attachments.values())
+let attachments = ""
+
+for (let i = 0; i < attachments_array.length; i++) {
+    attachments += `${attachments_array[i].url}\n`;
+}
+send(client.channels.cache.get(logs_channel), `[<t:${Math.round(time)}:f>] in ${channel.name}; **[${author.tag}](<${msg_url}>)**: ${content}\n${attachments}`);
+
+if (content.slice(0, 6) === "yo vel") {
+    if (channel.id === xinlaza_esan) {
+        send(channel, "Sorry, can't answer that here!")
         return;
     }
-
-    let content = message.content;
-    let author = message.author.tag;
-    let channel = message.channelId;
-    let time = message.createdTimestamp / 1000;
-    let msg_url = message.url;
-    let attachments_array = Array.from(message.attachments.values())
-    let attachments = ""
-    for (let i = 0; i < attachments_array.length; i++) {
-        attachments += `${attachments_array[i].url}\n`;
-    }
-    client.channels.cache.get(logs_channel).send(`[<t:${Math.round(time)}:f>] in <#${channel}>; **[${author}](<${msg_url}>)**: ${content}\n${attachments}`);
-
-    if (message.content.substring(0,6) == 'yo vel') {
-        switch (message.content.substring(7,12)) {
-            case 'xnlz ':
-                message.channel.send(`**${message.author}**:\n${
-                    search_xinlaza(
-                        message.content.substring(
-                            12, message.content.length
-                        )
-                    )
-                }`)
+    let command = content.split(" ");
+    if (command.length >= 3) {
+        let args = command.slice(3, command.length);
+        switch (command[2]) {
+            case 'xinlaza':
+            case 'xnlz':
+            case 'xlz':
+            case 'xz':
+                send(channel, xinlaza(args))
                 break;
-            case 'engl ':
-                message.channel.send(`**${message.author}**:\n${
-                    search_english(
-                        message.content.substring(
-                            12, message.content.length
-                        )
-                    )
-                }`)
-            case 'dict ':
-                message.channel.send("Sorry, this command isn't quite ready yet! How about we write some definitions?")
+            case 'english':
+            case 'engl':
+            case 'eng':
+            case 'en':
+                send(channel, english(args))
                 break;
-            case 'help!':
-                message.channel.send("Here's a memory refresher!\nYou invoke me with \`yo vel\`, if you don't write that first I won't respond. Sorry, it's just a quirk I have so I don't butt in too much!\nAfter you got my attention, here's what I can do:\n- \`yo vel xnlz <xz_word>\`: to search for a Xinlaza word. I'll give you up to 50 words that match your query.\n- \`yo vel engl <en_word>\`: the same, but in English. This time, spaces matter, so that you don't search for \'run\' and get B**run**ei, yk?\n- \`yo vel help!\`: this one invokes this reference doc that tells you every command available\n- \`yo vel dict <xz_word>\`: not quite ready yet! Once I update my database, you'll be able to get custom-made, native Xinlaza definitions of words instead of English translations! Exciting!!\n- \`yo vel code!\`/\`yo vel srce!\`: these ones are to see my source code in GitHub.\nWhat else can I help you with?")
+            case 'dict':
+                send(channel, "Sorry, this command isn't quite ready yet! How about we write some definitions?")
+                break;
+            case 'help':
+                send(channel, "Here's a memory refresher!\nYou invoke me with \`yo vel\`, if you don't write that first I won't respond. Sorry, it's just a quirk I have so I don't butt in too much!\nAfter you got my attention, here's what I can do:\n- \`yo vel xnlz <xz_word> [<parameters>]\`: searches for xinlaza words; accepts\n- \`yo vel engl <en_word>\`: the same, but in English. This time, spaces matter, so that you don't search for \'run\' and get B**run**ei, yk?\n- \`yo vel help!\`: this one invokes this reference doc that tells you every command available\n- \`yo vel dict <xz_word>\`: not quite ready yet! Once I update my database, you'll be able to get custom-made, native Xinlaza definitions of words instead of English translations! Exciting!!\n- \`yo vel code!\`/\`yo vel srce!\`: these ones are to see my source code in GitHub.\nWhat else can I help you with?")
                 break;
             case 'code':
-                message.channel.send("My crib? https://github.com/Matalya/xinlaza-bot. Why? Are you visiting? 7u7");
-            case 'srce!':
-                message.channel.send("My crib? https://github.com/Matalya/xinlaza-bot. Why? Are you visiting? 7u7");
-        };
+            case 'source':
+                send(channel, "My crib? https://github.com/Matalya/xinlaza-bot. Why? Are you visiting? 7u7");
+        }
+    } else {
+        send(channel, "Yes? :eyes:")
     }
-});
+}
+
+})
 
 // Log in to Discord with your client's token
 client.login(token);
